6bf6c0763173cb8295d2e94f92b0da06932e3b3b
BeLeap <beleap@beleap.dev>
2025-11-09 22:31:22 +0900

    update ingest command

--- rag/cmd/ingest.py
+++ rag/cmd/ingest.py
@@ -1,3 +1,15 @@
-def ingest():
-    print("Hello from ingest!")
+import argparse
+import glob
+
+def main():
+    parser = argparse.ArgumentParser(prog="ingest")
+    parser.add_argument("--path")
+
+    args = parser.parse_args()
+
+    target_files = glob.glob(args.path)
+    print(f"{len(target_files)} files will be ingested!")
+
+if __name__ == "__main__":
+    main()
 
